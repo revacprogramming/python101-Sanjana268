827d0d629be3a8fecf8287543bfe168ace3a83af
Sanjana Ramesh <95674920+Sanjana268@users.noreply.github.com>
2022-05-09 17:39:14 +0000

    05 done

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,22 @@
 # Functions
+def computepay(hours,rate):
+    print("In computepay",hours,rate)
+    if hours>40:
+    #print("overtime")
+        reg= hours*rate
+        otp= (hours-40.0) * (rate*0.5)
+        pay= reg + otp
+    else:
+        pay= hours*rate
+    #print("Returning", pay)
+    return pay
 
+sh= input("Enter hours:")
+sr= input("Enter rate per hour:")
+fh= float(sh)
+fr= float(sr)
+xp= computepay(fh,fr)
+#xp= fh*fr
 
-def computepay(h, r):
-    pass  # ...
+print("Pay",xp)   
 
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
