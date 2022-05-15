a3f42e7f7cd278a1eb31824b3e313e8b2c5cf596
Sanjana Ramesh <95674920+Sanjana268@users.noreply.github.com>
2022-05-15 08:01:18 +0000

    07 updated

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,6 +1,10 @@
 # Strings
+#text = "X-DSPAM-Confidence:    0.8475"
+def find_pos(tex): 
+    x=tex.find(':')
+    y=tex[x+1:]
+    z=float(y)
+    print(z)
+
 text = "X-DSPAM-Confidence:    0.8475"
-pos=text.find(':')
-x=text[pos+1:]
-y=float(x)
-print(y)
+pos=find_pos(text)
