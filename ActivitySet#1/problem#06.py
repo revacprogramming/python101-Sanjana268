42c3206aa7a7c7261e6c2464889632f2555abf79
Sanjana Ramesh <95674920+Sanjana268@users.noreply.github.com>
2022-05-31 17:20:19 +0000

    05 done

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,6 +1,7 @@
 # Loops & Iterators
 
 def find_maxmin(largest,smallest):
+def  
     while True:
         num=input("Enter a number:")
         if num=='done':break
@@ -13,13 +14,28 @@ def find_maxmin(largest,smallest):
       
         except:
            print("Invalid input")
-           continue
+           print("Invalid input")
+           print("Invalid input")
+          continue:
     print("Maximum is",largest)
     print("Minimum is",smallest)
+
 
 x=0
 y=0
 z=find_maxmin(x,y)
 #print("Maximum,Minimum",x,y)
+
+largest=0
+smallest=0
+while True:
+  while True:
+    num=input("Enter number:")
+    if num=='done'
+    try:
+      num=int(num)
+      num=='done':bre
+    
+    
 
 
